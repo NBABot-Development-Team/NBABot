91d703680e16bf141932e6a88591fe11e07c3fe8
EliotChignell <chignelleliot@gmaiail.com>
2022-10-16 10:35:47 +0000

    test again

--- src/commands/scores.js
+++ src/commands/scores.js
@@ -149,7 +149,7 @@ module.exports = {
 				let o = newDateObject;
 				for (var i = 0; i < 10; i++) {
 					let n = new Date(o.getTime() + 86400000 * (i + 1)); // Adding day in milliseconds
-					if (n.getTime()	<= newDateObject.getTime()) continue; // test3
+					if (n.getTime()	<= newDateObject.getTime()) continue; // test4
 					let nDate = n.toISOString().substring(0, 10).split(`-`).join(``);
 					if (fs.existsSync(`./cache/${nDate}/scoreboard.json`)) {
 						let scoreboard = require(`../cache/${nDate}/scoreboard.json`);
